c7878522626c37b9d8216237b720c3d87018000e
nur <nur@NURTURANBAC8>
2022-12-12 09:59:52 +0300

    .

--- gezen/gezen/Form1.Designer.cs
+++ gezen/gezen/Form1.Designer.cs
@@ -41,12 +41,14 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.Location = new System.Drawing.Point(93, 249);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 53);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
