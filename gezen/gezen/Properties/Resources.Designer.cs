e2d9d23d0b05a823388e2ba997538fb6b8297d13
nur <nur@NURTURANBAC8>
2022-12-12 13:33:50 +0300

    görsel eklendi

--- gezen/gezen/Properties/Resources.Designer.cs
+++ gezen/gezen/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace gezen.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap anıtkabir3 {
+            get {
+                object obj = ResourceManager.GetObject("anıtkabir3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
