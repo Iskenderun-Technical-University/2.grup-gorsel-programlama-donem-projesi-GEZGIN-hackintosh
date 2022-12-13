4cd785f8e18ef85f361f5077f4c0faaf7ed5e706
Onur Berkant GİRGEÇ <95904801+OnruBekrant@users.noreply.github.com>
2022-12-14 00:46:31 +0300

    Add files via upload
    panel2.Backgroundimage

---
+++ gezen_onur/gezen_V2/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace gezen_V2
+{
+    partial class Form_ankara
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_denemeankara = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox_denemeankara
+            // 
+            this.listBox_denemeankara.FormattingEnabled = true;
+            this.listBox_denemeankara.ItemHeight = 15;
+            this.listBox_denemeankara.Location = new System.Drawing.Point(111, 210);
+            this.listBox_denemeankara.Name = "listBox_denemeankara";
+            this.listBox_denemeankara.Size = new System.Drawing.Size(120, 94);
+            this.listBox_denemeankara.TabIndex = 0;
+            // 
+            // Form_ankara
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox_denemeankara);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_ankara";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox_denemeankara;
+    }
+}
---
+++ gezen_onur/gezen_V2/Form3.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace gezen_V2
+{
+    partial class Form_istanbul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_test_istanbul = new System.Windows.Forms.Button();
+            this.label_test_ist = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_test_istanbul
+            // 
+            this.button_test_istanbul.Location = new System.Drawing.Point(236, 129);
+            this.button_test_istanbul.Name = "button_test_istanbul";
+            this.button_test_istanbul.Size = new System.Drawing.Size(75, 23);
+            this.button_test_istanbul.TabIndex = 0;
+            this.button_test_istanbul.Text = "hjjjhg";
+            this.button_test_istanbul.UseVisualStyleBackColor = true;
+            // 
+            // label_test_ist
+            // 
+            this.label_test_ist.AutoSize = true;
+            this.label_test_ist.Location = new System.Drawing.Point(301, 173);
+            this.label_test_ist.Name = "label_test_ist";
+            this.label_test_ist.Size = new System.Drawing.Size(38, 15);
+            this.label_test_ist.TabIndex = 1;
+            this.label_test_ist.Text = "label1";
+            // 
+            // Form_istanbul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_test_ist);
+            this.Controls.Add(this.button_test_istanbul);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_istanbul";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button_test_istanbul;
+        private Label label_test_ist;
+    }
+}
